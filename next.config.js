8861973a353de8d86f74c9c4be8b1bd8933eb1d5
khabibkhanov <hamidulloxon2002@gmail.com>
2022-12-19 17:46:34 +0500

    turn off eslint

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, "./src/assets/scss")],
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
     images: {
         domains: ["localhost"],
     },
