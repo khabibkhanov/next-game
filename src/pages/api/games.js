3cfc48e1210e78073af4e29cb2d5a614ff46b1e0
khabibkhanov <hamidulloxon2002@gmail.com>
2023-01-25 16:51:36 +0500

    is working?

--- src/pages/api/games.js
+++ src/pages/api/games.js
@@ -3,9 +3,8 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-    await axios.get("http://localhost:1337/api/names")
-    .then(response => {
-        let dt = response.data;
-        res.status(200).json({ games: dt });
-    })
+
+    console.log('Entered the serverless function')
+
+    return { "dummy": "data"}
 }
